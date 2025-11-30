c520a611f2f282f9c42420ab79a2ed2ceadf4868
Kledi <kledikiki@gmail.com>
2025-11-30 14:36:20 +0100

    Added labels to the input areas

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,7 +12,9 @@ function Form(){
     return (
         <>
             <div id="Form">
+            <label>Enter your name: </label>
             <input type="text" placeholder="Name" onChange={(e) => {setInputName(e.target.value)}}/>
+            <label>Enter the time you want: </label>
             <input type="time" placeholder="Name" onChange={(e) => {setInputTime(e.target.value)}}/>
             <button onClick={() => {SetForm([{name: inputName, time: inputTime}])}}>Submit</button>
             <p>Your name is: {form[0].name}</p>
